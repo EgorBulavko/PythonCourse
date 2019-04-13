ffb87299185d23c07ff055f38637f9cd6cb1c033
EgorBulavko <48317055+EgorBulavko@users.noreply.github.com>
2019-04-13 14:11:50 +0300

    Update hw_E1.py

--- hw_E1.py
+++ hw_E1.py
@@ -1,5 +1,4 @@
 Пуассон = np.random.poisson(4,3)
 import seaborn as sns
-sns.distplot(Пуассон, label = 'Пуассоновское distribution')
-plt.legend()
-plt.show()
+sns.distplot(Пуассон, label = 'Пуассоновское distribution'), plt.legend(), plt.show()
+
